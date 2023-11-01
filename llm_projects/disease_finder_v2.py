2150be1db7bb1e9ad071c0db3d0b4297e1f07d4d
smttsp <samettaspinarx@gmail.com>
2023-11-01 12:52:50 -0400

    [UPD] fixed problems with v2

--- llm_projects/disease_finder_v2.py
+++ llm_projects/disease_finder_v2.py
@@ -50,67 +50,27 @@ def get_vectorstore(file_path, persist_directory=VECTOR_DB_DIRECTORY):
         OpenAIEmbeddings(),
     )
 
-    index_id = 0
-    #
-    # embedding_vector2 = vector_db2.index.reconstruct_n(index_id, 1)[0]
-    embeds = vector_db.index.reconstruct_n(index_id, 1)[0]
-    # embeds = vector_db._collection.get(include=['embeddings', "metadatas"])
-    print(embeds[:3])
-
-    # vector_db2 = FAISS.from_documents(
-    #     documents=docs, embedding=OpenAIEmbeddingsLower()
-    # )
-    # embeds2 = vector_db2.index.reconstruct_n(index_id, 1)[0]
-    # # embeds2 = vector_db2._collection.get(include=['embeddings', "metadatas"])
-    # print(embeds2[:3])
-
     # vectordb._persist_directory = persist_directory
     return vector_db
 
 
 def disease_finder_v2():
-    vector_db = get_vectorstore("data/test.csv")
+    vector_db = get_vectorstore("data/train.csv")
     df = pandas.read_csv("data/test.csv")
 
-    # embeds = vector_db._collection.get(include=['embeddings', "metadatas"])
-    oa_embedder = OpenAIEmbeddings()
-    # oa_embedder = OpenAIEmbeddingsLower()
+    k = 5
+    cnts = numpy.zeros(k)
 
-    # cur_text = df.text[idx]
-    # txt_embed = oa_embedder.embed_query(cur_text)
-    # embedding_vector = vector_db.index.reconstruct_n(idx, 1)[0]
-    mx = -1
-
-    idx = 0
-    cur_text = df.text[idx]
-    print(df.label[idx], "---", cur_text)
-    txt_embed = oa_embedder.embed_query(cur_text)
-    index_to_docstore_id = vector_db.index_to_docstore_id
-    for idx in range(len(df)):
-        embedding_vector = vector_db.index.reconstruct_n(idx, 1)[0]
-        metadata = vector_db.docstore._dict[index_to_docstore_id[idx]]
-        cur_cor = numpy.dot(embedding_vector, txt_embed)
-        if cur_cor > mx:
-            mx = cur_cor
-            print(cur_cor, df.label[idx], "---", cur_text)
-            print("*" * 30)
-            print()
-
-
-    cnt, cnt2, cnt3 = 0, 0, 0
     top_n = 10
     for text, label in zip(df.text, df.label):
-
-        # res = vector_db.get_relevant_documents(text, top_n)
-        top_n_results = vector_db.max_marginal_relevance_search(text.lower(), fetch_k=top_n, k=3)
+        top_n_results = vector_db.max_marginal_relevance_search(text, fetch_k=top_n, k=k)
         pred_labels = [doc.metadata.get("label") for doc in top_n_results]
         print(f"{label=}, {pred_labels=}")
 
-        # print("*" * 30)
-        cnt += int(label in pred_labels)
-        cnt2 += int(pred_labels.count(label) >= 2)
-        cnt3 += int(pred_labels.count(label) >= 3)
+        for i in range(k):
+            cnts[i] += int(pred_labels.count(label) > i)
 
-    print("at least one match", cnt / len(df))
-    print("at least two matches", cnt2 / len(df))
-    print("all matched", cnt3 / len(df))
+    for i in range(k):
+        print(f"at least {i+1} match", cnts[i] / len(df))
+
+    return cnts / len(df)
