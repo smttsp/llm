63d7985f6b91fcc678dfc0eb35b6265891d6e267
smttsp <samettaspinarx@gmail.com>
2023-10-31 23:47:52 -0400

    [UPD] v3 guidelines added

---
+++ llm_projects/disease_finder_v3.py
@@ -0,0 +1,12 @@
+"""This one creates a vectorstore from the training data by preprocessing the
+input text using the two common preprocessing steps:
+
+# https://chat.openai.com/share/4ada2e1b-7df8-4039-aab3-7ffef441ea3e
+
+1. Lowercase
+2. Remove stopwords
+3. Lemmatize
+
+"""
+
+
