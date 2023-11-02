9f28a420f574fbc8430810ba7b4d06189a9bfd43
smttsp <samettaspinarx@gmail.com>
2023-11-01 20:49:41 -0400

    [UPD] disease_finder_v3, lowercase, stopword, lemmatize done
    all degrades the performance

--- llm_projects/disease_finder/disease_finder_v3.py
+++ llm_projects/disease_finder/disease_finder_v3.py
@@ -8,6 +8,94 @@ input text using the two common preprocessing steps:
 3. Lemmatize
 
 """
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+
+# Download the NLTK stop words data
+nltk.download('stopwords')
+nltk.download('punkt')
+
+# Get the list of English stop words
+STOP_WORDS = set(stopwords.words('english'))
+
+# Download the WordNet lemmatizer data
+nltk.download('wordnet')
+
+# Initialize the lemmatizer
+LEMMATIZER = WordNetLemmatizer()
+
+
+def lemmatize(text, lemmatizer=None):
+    if lemmatizer is None:
+        lemmatizer = LEMMATIZER
+
+    # Tokenize the sentence into words
+    words = nltk.word_tokenize(text)
+
+    # Lemmatize each word in the sentence
+    lemmatized_words = [lemmatizer.lemmatize(word) for word in words]
+
+    # Join the lemmatized words back into a sentence
+    lemmatized_sentence = ' '.join(lemmatized_words)
+
+    # Print the lemmatized sentence
+    # print(lemmatized_sentence)
+    return lemmatized_sentence
+
+
+def remove_stopwords(text, stop_words=None):
+    if stop_words is None:
+        stop_words = STOP_WORDS
+
+    # Tokenize the sentence into words
+    words = word_tokenize(text)
+
+    # Remove stop words from the tokenized words
+    filtered_words = [word for word in words if word.lower() not in stop_words]
+
+    # Join the filtered words back into a sentence
+    filtered_sentence = ' '.join(filtered_words)
+
+    # Print the filtered sentence
+    # print(filtered_sentence)
+    return filtered_sentence
+
+
+class OpenAIEmbeddingsStopWord(OpenAIEmbeddings):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def embed_query(self, text):
+        preprocessed_text = remove_stopwords(text)
+        # Call the original embed_query method with the preprocessed text
+        return super().embed_query(preprocessed_text)
+
+    def embed_documents(self, texts, **kwargs):
+        preprocessed_texts = [remove_stopwords(text) for text in texts]
+        # Call the original embed_query method with the preprocessed text
+        return super().embed_documents(preprocessed_texts, **kwargs)
+
+
+class OpenAIEmbeddingsLemmatize(OpenAIEmbeddings):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def embed_query(self, text):
+        preprocessed_text = lemmatize(text)
+        # Call the original embed_query method with the preprocessed text
+        return super().embed_query(preprocessed_text)
+
+    def embed_documents(self, texts, **kwargs):
+        preprocessed_texts = [lemmatize(text) for text in texts]
+        # Call the original embed_query method with the preprocessed text
+        return super().embed_documents(preprocessed_texts, **kwargs)
+
+
 
 class OpenAIEmbeddingsLower(OpenAIEmbeddings):
     def __init__(self, **kwargs):
@@ -26,6 +114,7 @@ class OpenAIEmbeddingsLower(OpenAIEmbeddings):
         preprocessed_texts = [self.preprocess_text(text) for text in texts]
         # Call the original embed_query method with the preprocessed text
         return super().embed_documents(preprocessed_texts, **kwargs)
+
 
 
 # # Example usage
