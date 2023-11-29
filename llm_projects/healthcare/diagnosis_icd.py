d57218d01864c308be58009100c05a53eb7e215a
smttsp <samettaspinarx@gmail.com>
2023-11-29 11:24:29 -0500

    [UPD] omr plots

---
+++ llm_projects/healthcare/diagnosis_icd.py
@@ -0,0 +1,12 @@
+import pandas
+from .generic_plots import plot_distributions
+
+
+def get_diagnosis_statistics(diagnoses_icd_df: pandas.DataFrame):
+    grouped_data = diagnoses_icd_df.groupby(
+        ['subject_id', 'hadm_id']).size().reset_index(name='counts'
+    )
+
+    # print(grouped_data)
+
+    plot_distributions(grouped_data["counts"], "num_icd_codes", "admission")
---
+++ llm_projects/healthcare/omr.py
@@ -0,0 +1,12 @@
+from .generic_plots import plot_distributions
+
+
+def get_omr_statistics(omr_df):
+    bmi_list = omr_df[
+        omr_df["result_name"].isin(["BMI", 'BMI (kg/m2)'])
+    ].result_value.to_list()
+
+    # bmi_list = list(map(float, bmi_list))
+    bmi_list = [float(val) for val in bmi_list if float(val) < 80]
+
+    plot_distributions(bmi_list, "BMI", "entry")
