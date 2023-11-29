51d7a54eb4582e0730ec3421be71fba09c4b4ed2
smttsp <samettaspinarx@gmail.com>
2023-11-28 19:32:03 -0500

    [UPD] services are evaluated

---
+++ llm_projects/healthcare/__init__.py
@@ -0,0 +1,5 @@
+# from read_mimic import read_csvs
+
+from llm_projects.healthcare.patients import get_patient_statistics
+from llm_projects.healthcare.admissions import get_admissions_statistics
+from llm_projects.healthcare.services import get_services_statistics
