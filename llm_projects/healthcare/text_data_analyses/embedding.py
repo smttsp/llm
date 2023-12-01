99b7a130cef6ce3f3513d275dbc7c89116982675
smttsp <samettaspinarx@gmail.com>
2023-12-01 17:24:00 -0500

    [GIT] gitignore updated, commended out redundant func

--- llm_projects/healthcare/text_data_analyses/embedding.py
+++ llm_projects/healthcare/text_data_analyses/embedding.py
@@ -56,28 +56,28 @@ def embedder_test(docs):
     return None
 
 
-def get_embeddings(df):
-    df = df.drop_duplicates(subset=["subject_id"])
-
-    embedder = OpenAIEmbeddings()
-
-    embeds = []
-    for idx, row in df.iterrows():
-        # print(idx, row)
-        embed = embedder.embed_query(row["text"])
-        embeds.append(embed)
-
-        if idx == 100:
-            break
-
-    sz = len(embeds)
-    cor2 = numpy.zeros((sz, sz))
-
-    for i in range(len(embeds)):
-        for j in range(i + 1, len(embeds)):
-            cor2[i][j] = numpy.dot(embeds[i], embeds[j])
-            cor2[j][i] = cor2[i][j]
-
-    x = cor2.max(axis=1)
-    print(x)
-    return None
+# def get_embeddings(df):
+#     df = df.drop_duplicates(subset=["subject_id"])
+#
+#     embedder = OpenAIEmbeddings()
+#
+#     embeds = []
+#     for idx, row in df.iterrows():
+#         # print(idx, row)
+#         embed = embedder.embed_query(row["text"])
+#         embeds.append(embed)
+#
+#         if idx == 100:
+#             break
+#
+#     sz = len(embeds)
+#     cor2 = numpy.zeros((sz, sz))
+#
+#     for i in range(len(embeds)):
+#         for j in range(i + 1, len(embeds)):
+#             cor2[i][j] = numpy.dot(embeds[i], embeds[j])
+#             cor2[j][i] = cor2[i][j]
+#
+#     x = cor2.max(axis=1)
+#     print(x)
+#     return None
